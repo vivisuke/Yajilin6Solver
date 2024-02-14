4950bc1a18dab56b88ab609028e9e0f226ebd96f
vivisuke <ntsuda@master.email.ne.jp>
2024-02-15 08:20:51 +0900

    0012 ◎ C 02/15 02/15 0.001│ToDo│Board6x6：ループランダム生成１ステップ

--- Yajilin6Solver/classes/Board6x6.gd
+++ Yajilin6Solver/classes/Board6x6.gd
@@ -18,12 +18,14 @@ const LINE_LEFT = 8
 const DOT = 16
 
 var ary_board = []
+var dir_order
 
 func xyToIX(x, y):		# x, y: [0, N_HORZ)
 	return x + (y+1) * ARY_WIDTH
 func is_line(ix) -> bool:
 	return ary_board[ix] > 0
 func _init():
+	dir_order = [LINE_UP, LINE_DOWN, LINE_LEFT, LINE_RIGHT]
 	ary_board.resize(ARY_SIZE)
 	make_loop()
 	#ary_board[xyToIX(0, 0)] = LINE_RIGHT | LINE_DOWN
@@ -69,6 +71,7 @@ func move_line2_right(ix) -> bool:
 func move_line2_left(ix) -> bool:
 	if ary_board[ix+ARY_WIDTH] != EMPTY || !is_line(ix+1) || !is_line(ix+ARY_WIDTH+1):
 		return false
+	if (ary_board[ix+1] & LINE_DOWN) == 0: return false		# 線分が連続していない場合
 	ary_board[ix] = LINE_RIGHT | LINE_DOWN
 	ary_board[ix+ARY_WIDTH] = LINE_RIGHT | LINE_UP
 	ary_board[ix+1] ^= LINE_LEFT | LINE_DOWN
@@ -79,6 +82,7 @@ func move_line2_left(ix) -> bool:
 func move_line2_up(ix) -> bool:
 	if ary_board[ix+1] != EMPTY || !is_line(ix+ARY_WIDTH) || !is_line(ix+ARY_WIDTH+1):
 		return false
+	if (ary_board[ix+ARY_WIDTH] & LINE_RIGHT) == 0: return false		# 線分が連続していない場合
 	ary_board[ix] = LINE_RIGHT | LINE_DOWN
 	ary_board[ix+1] = LINE_LEFT | LINE_DOWN
 	ary_board[ix+ARY_WIDTH] ^= LINE_RIGHT | LINE_UP
@@ -89,17 +93,29 @@ func move_line2_up(ix) -> bool:
 func move_line2_down(ix) -> bool:
 	if ary_board[ix+1] != EMPTY || !is_line(ix-ARY_WIDTH) || !is_line(ix-ARY_WIDTH+1):
 		return false
+	if (ary_board[ix-ARY_WIDTH] & LINE_RIGHT) == 0: return false		# 線分が連続していない場合
 	ary_board[ix] = LINE_RIGHT | LINE_UP
 	ary_board[ix+1] = LINE_LEFT | LINE_UP
 	ary_board[ix-ARY_WIDTH] ^= LINE_RIGHT | LINE_DOWN
 	ary_board[ix-ARY_WIDTH+1] ^= LINE_LEFT | LINE_DOWN
 	return true
-func move_line():
-	move_line2_up(xyToIX(1, 0))
-	move_line2_down(xyToIX(2, 5))
-	move_line2_left(xyToIX(0, 2))
-	move_line2_right(xyToIX(5, 1))
-	move_line2_right(xyToIX(2, 2))
+func move_line(ix) -> bool:
+	dir_order.shuffle()
+	for i in range(dir_order.size()):
+		if dir_order[i] == LINE_UP:
+			if move_line2_up(ix): return true
+		if dir_order[i] == LINE_DOWN:
+			if move_line2_down(ix): return true
+		if dir_order[i] == LINE_LEFT:
+			if move_line2_left(ix): return true
+		if dir_order[i] == LINE_RIGHT:
+			if move_line2_right(ix): return true
+	#move_line2_up(xyToIX(1, 0))
+	#move_line2_down(xyToIX(2, 5))
+	#move_line2_left(xyToIX(0, 2))
+	#move_line2_right(xyToIX(5, 1))
+	#move_line2_right(xyToIX(2, 2))
+	return false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
--- Yajilin6Solver/scripts/main.gd
+++ Yajilin6Solver/scripts/main.gd
@@ -1,8 +1,16 @@
 extends Node2D
 
+const N_HORZ = 6
+const N_VERT = 6
+const ARY_WIDTH = N_HORZ + 1
+const ARY_HEIGHT = N_VERT + 2
+const ARY_SIZE = ARY_WIDTH * ARY_HEIGHT
+
 var CBoard6x6 = preload("res://classes/Board6x6.gd")
 var bd
 
+func xyToIX(x, y):		# x, y: [0, N_HORZ)
+	return x + (y+1) * ARY_WIDTH
 func _ready():
 	bd = CBoard6x6.new()
 	$Board/LineLayer.ary_board = bd.ary_board
@@ -11,7 +19,9 @@ func _ready():
 
 func _input(event):
 	if event is InputEventMouseButton && event.is_pressed():
-		bd.move_line()
+		var mp = $Board/TileMap.local_to_map($Board/TileMap.get_local_mouse_position())
+		print(mp)
+		bd.move_line(xyToIX(mp[0], mp[1]))
 		$Board/LineLayer.queue_redraw()
 
 func _process(delta):
